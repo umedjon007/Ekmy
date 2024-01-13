eba64c1aa7110facc8f9b2aa817ff99e36a77832
umedjon007 <153191098+umedjon007@users.noreply.github.com>
2024-01-13 20:02:18 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '320px',
+
+      'md': '600px',
+
+      'lg': '830px',
+
+      'xl': '1080px',
+
+      '2xl': '1430px',
+    }
+  },
+  plugins: [],
+}
